9783851bea1559d8870c7fa4c4013ba8363c28b9
Osempu <oscared94@outlook.com>
2025-04-07 23:43:31 -0500

    Added gitignore and readme

--- obj/Debug/net8.0/PassGen.AssemblyInfo.cs
+++ obj/Debug/net8.0/PassGen.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PassGen")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+869366b0bdf10c1322b3e246353a5f15b8b72ce3")]
 [assembly: System.Reflection.AssemblyProductAttribute("PassGen")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PassGen")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
